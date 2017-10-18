6099feca11a971dbd9af45f166d12b03a2d5dcfe
Shereen Messi <shereenmessi@hotmail.com>
2017-10-18 19:09:58 +0300

    Get Data from Api

--- JSONApp/app/src/main/java/com/example/android/jsonapp/MainActivity.java
+++ JSONApp/app/src/main/java/com/example/android/jsonapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.jsonapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -17,193 +22,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        String jsonData = "{\n" +
-                "  \"type\": \"FeatureCollection\",\n" +
-                "  \"metadata\": {\n" +
-                "    \"generated\": 1508237335000,\n" +
-                "    \"url\": \"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson\",\n" +
-                "    \"title\": \"USGS All Earthquakes, Past Hour\",\n" +
-                "    \"status\": 200,\n" +
-                "    \"api\": \"1.5.8\",\n" +
-                "    \"count\": 4\n" +
-                "  },\n" +
-                "  \"features\": [\n" +
-                "    {\n" +
-                "      \"type\": \"Feature\",\n" +
-                "      \"properties\": {\n" +
-                "        \"mag\": 0.1,\n" +
-                "        \"place\": \"53km ESE of Beatty, Nevada\",\n" +
-                "        \"time\": 1508236792090,\n" +
-                "        \"updated\": 1508236871836,\n" +
-                "        \"tz\": -480,\n" +
-                "        \"url\": \"https://earthquake.usgs.gov/earthquakes/eventpage/nn00609554\",\n" +
-                "        \"detail\": \"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nn00609554.geojson\",\n" +
-                "        \"felt\": null,\n" +
-                "        \"cdi\": null,\n" +
-                "        \"mmi\": null,\n" +
-                "        \"alert\": null,\n" +
-                "        \"status\": \"automatic\",\n" +
-                "        \"tsunami\": 0,\n" +
-                "        \"sig\": 0,\n" +
-                "        \"net\": \"nn\",\n" +
-                "        \"code\": \"00609554\",\n" +
-                "        \"ids\": \",nn00609554,\",\n" +
-                "        \"sources\": \",nn,\",\n" +
-                "        \"types\": \",geoserve,origin,phase-data,\",\n" +
-                "        \"nst\": 9,\n" +
-                "        \"dmin\": 0.13,\n" +
-                "        \"rms\": null,\n" +
-                "        \"gap\": 112.86,\n" +
-                "        \"magType\": \"ml\",\n" +
-                "        \"type\": \"earthquake\",\n" +
-                "        \"title\": \"M 0.1 - 53km ESE of Beatty, Nevada\"\n" +
-                "      },\n" +
-                "      \"geometry\": {\n" +
-                "        \"type\": \"Point\",\n" +
-                "        \"coordinates\": [\n" +
-                "          -116.2542,\n" +
-                "          36.6482,\n" +
-                "          16.6\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      \"id\": \"nn00609554\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"type\": \"Feature\",\n" +
-                "      \"properties\": {\n" +
-                "        \"mag\": 0.8,\n" +
-                "        \"place\": \"14km NNE of North Pole, Alaska\",\n" +
-                "        \"time\": 1508235720159,\n" +
-                "        \"updated\": 1508236102809,\n" +
-                "        \"tz\": -540,\n" +
-                "        \"url\": \"https://earthquake.usgs.gov/earthquakes/eventpage/ak17063159\",\n" +
-                "        \"detail\": \"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ak17063159.geojson\",\n" +
-                "        \"felt\": null,\n" +
-                "        \"cdi\": null,\n" +
-                "        \"mmi\": null,\n" +
-                "        \"alert\": null,\n" +
-                "        \"status\": \"automatic\",\n" +
-                "        \"tsunami\": 0,\n" +
-                "        \"sig\": 10,\n" +
-                "        \"net\": \"ak\",\n" +
-                "        \"code\": \"17063159\",\n" +
-                "        \"ids\": \",ak17063159,\",\n" +
-                "        \"sources\": \",ak,\",\n" +
-                "        \"types\": \",geoserve,origin,\",\n" +
-                "        \"nst\": null,\n" +
-                "        \"dmin\": null,\n" +
-                "        \"rms\": 0.5,\n" +
-                "        \"gap\": null,\n" +
-                "        \"magType\": \"ml\",\n" +
-                "        \"type\": \"earthquake\",\n" +
-                "        \"title\": \"M 0.8 - 14km NNE of North Pole, Alaska\"\n" +
-                "      },\n" +
-                "      \"geometry\": {\n" +
-                "        \"type\": \"Point\",\n" +
-                "        \"coordinates\": [\n" +
-                "          -147.26,\n" +
-                "          64.8724,\n" +
-                "          4.5\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      \"id\": \"ak17063159\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"type\": \"Feature\",\n" +
-                "      \"properties\": {\n" +
-                "        \"mag\": 2.05,\n" +
-                "        \"place\": \"17km NE of Olancha, CA\",\n" +
-                "        \"time\": 1508235520200,\n" +
-                "        \"updated\": 1508235738772,\n" +
-                "        \"tz\": -480,\n" +
-                "        \"url\": \"https://earthquake.usgs.gov/earthquakes/eventpage/ci38026616\",\n" +
-                "        \"detail\": \"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ci38026616.geojson\",\n" +
-                "        \"felt\": null,\n" +
-                "        \"cdi\": null,\n" +
-                "        \"mmi\": null,\n" +
-                "        \"alert\": null,\n" +
-                "        \"status\": \"automatic\",\n" +
-                "        \"tsunami\": 0,\n" +
-                "        \"sig\": 65,\n" +
-                "        \"net\": \"ci\",\n" +
-                "        \"code\": \"38026616\",\n" +
-                "        \"ids\": \",ci38026616,\",\n" +
-                "        \"sources\": \",ci,\",\n" +
-                "        \"types\": \",geoserve,nearby-cities,origin,phase-data,\",\n" +
-                "        \"nst\": 24,\n" +
-                "        \"dmin\": 0.1757,\n" +
-                "        \"rms\": 0.18,\n" +
-                "        \"gap\": 107,\n" +
-                "        \"magType\": \"ml\",\n" +
-                "        \"type\": \"earthquake\",\n" +
-                "        \"title\": \"M 2.1 - 17km NE of Olancha, CA\"\n" +
-                "      },\n" +
-                "      \"geometry\": {\n" +
-                "        \"type\": \"Point\",\n" +
-                "        \"coordinates\": [\n" +
-                "          -117.8698333,\n" +
-                "          36.394,\n" +
-                "          0.42\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      \"id\": \"ci38026616\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"type\": \"Feature\",\n" +
-                "      \"properties\": {\n" +
-                "        \"mag\": 5,\n" +
-                "        \"place\": \"86km NW of Nuku`alofa, Tonga\",\n" +
-                "        \"time\": 1508234027880,\n" +
-                "        \"updated\": 1508235315040,\n" +
-                "        \"tz\": -720,\n" +
-                "        \"url\": \"https://earthquake.usgs.gov/earthquakes/eventpage/us1000at83\",\n" +
-                "        \"detail\": \"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us1000at83.geojson\",\n" +
-                "        \"felt\": null,\n" +
-                "        \"cdi\": null,\n" +
-                "        \"mmi\": null,\n" +
-                "        \"alert\": null,\n" +
-                "        \"status\": \"reviewed\",\n" +
-                "        \"tsunami\": 0,\n" +
-                "        \"sig\": 385,\n" +
-                "        \"net\": \"us\",\n" +
-                "        \"code\": \"1000at83\",\n" +
-                "        \"ids\": \",us1000at83,\",\n" +
-                "        \"sources\": \",us,\",\n" +
-                "        \"types\": \",geoserve,origin,phase-data,\",\n" +
-                "        \"nst\": null,\n" +
-                "        \"dmin\": 5.587,\n" +
-                "        \"rms\": 1.56,\n" +
-                "        \"gap\": 39,\n" +
-                "        \"magType\": \"mb\",\n" +
-                "        \"type\": \"earthquake\",\n" +
-                "        \"title\": \"M 5.0 - 86km NW of Nuku`alofa, Tonga\"\n" +
-                "      },\n" +
-                "      \"geometry\": {\n" +
-                "        \"type\": \"Point\",\n" +
-                "        \"coordinates\": [\n" +
-                "          -175.6702,\n" +
-                "          -20.489,\n" +
-                "          172.4\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      \"id\": \"us1000at83\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"bbox\": [\n" +
-                "    -175.6702,\n" +
-                "    -20.489,\n" +
-                "    0.42,\n" +
-                "    -116.2542,\n" +
-                "    64.8724,\n" +
-                "    172.4\n" +
-                "  ]\n" +
-                "}";
+
+        EarthQuakeAsyncTask task = new EarthQuakeAsyncTask();
+        task.execute();
 
 
+    }
+
+    private void DisplayResult(String jsonData) {
         ArrayList<EarthQuake> earthQuakes = EarthQuakeUtil.ExtractDat(jsonData);
         EarthQuakeArrayAdapter adapter = new EarthQuakeArrayAdapter(this, 0, earthQuakes);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+    }
 
+    class EarthQuakeAsyncTask extends AsyncTask {
+
+        @Override
+        protected String doInBackground(Object[] objects) {
+            StringBuilder JsonData = new StringBuilder();
+            HttpURLConnection httpURLConnection = null;
+            InputStream inputStream=null;
+            try {
+                URL url = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10");
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setConnectTimeout(10000);
+                httpURLConnection.setReadTimeout(15000);
+                httpURLConnection.connect();
+                inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    JsonData.append(line);
+                    line = reader.readLine();
+                }
+                Log.v("AsyncTask", "Connected" + httpURLConnection.getResponseCode());
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.v("AsyncTask", e.getMessage());
+            }
+            finally {
+                if (httpURLConnection != null)
+                    httpURLConnection.disconnect();
+                if(inputStream!=null)
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+            }
+
+            return JsonData.toString();
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            DisplayResult(o.toString());
+        }
     }
 }
